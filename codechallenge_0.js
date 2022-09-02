26e1514a5c5a1fc25b740669ebfc8175e8409961
Christopher Javier <82299283+ChristopherJavier@users.noreply.github.com>
2022-09-02 16:00:19 -0400

    Added the code challenge 0
    This is the solution for the Snack problem in BeeCrowd

---
+++ codechallenge_0.js
@@ -0,0 +1,28 @@
+const snackTotal = (x, y) => {
+    let price, total
+
+    switch (x) {
+        case 1:
+            price = 4.00;
+            break
+        case 2:
+            price = 4.50;
+            break
+        case 3:
+            price = 5.00;
+            break
+        case 4:
+            price = 2.00;
+            break
+        case 5:
+            price = 1.50;
+            break
+        default:
+            console.log("No valid code")
+    }
+
+    total = price * y;
+    return console.log(`Total: R$${total.toFixed(2)}`)
+}
+
+snackTotal(2, 3)
